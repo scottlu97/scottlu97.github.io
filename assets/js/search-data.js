055be79fabae6553ec83adadb67655a300d73e4f
scottlu97 <123441792+scottlu97@users.noreply.github.com>
2026-04-22 09:36:14 +0000

    Deploying to gh-pages from @ scottlu97/scottlu97.github.io@0355dfaf0286e18d26fe6b8e1965b1152ba5a8ca 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -11,17 +11,17 @@ ninja.data = [{
     },
   },{id: "nav-cv",
           title: "CV",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
+          description: "",
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
           },
-        },{id: "nav-teaching",
-          title: "teaching",
-          description: "Course materials, schedules, and resources for classes taught.",
+        },{id: "nav-research",
+          title: "Research",
+          description: "",
           section: "Navigation",
           handler: () => {
-            window.location.href = "/teaching/";
+            window.location.href = "/research/";
           },
         },{id: "post-a-post-with-plotly-js",
         
